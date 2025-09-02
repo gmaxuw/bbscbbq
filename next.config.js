e143c9152cdb068a0b731b41995d13dd219ed0f4
Gab BBQ Owner <gab@bbq-stalls.com>
2025-09-02 20:11:20 +0800

    🔧 Remove Edge Function completely to fix Next.js build

--- next.config.js
+++ next.config.js
@@ -8,14 +8,6 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
-  // Exclude Supabase Edge Functions from Next.js build
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /supabase\/functions\/.*\.ts$/,
-      use: 'ignore-loader',
-    });
-    return config;
-  },
 }
 
 module.exports = nextConfig
